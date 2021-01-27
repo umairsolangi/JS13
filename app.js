c3233f644a53b48bdc78915fe0f7378e7cd41985
umairsolangi <51870556+umairsolangi@users.noreply.github.com>
2021-01-27 10:33:54 +0500

    JS 13

---
+++ app.js
@@ -0,0 +1,97 @@
+// Changing case
+// Question No 01 
+
+// var input = prompt("Enter your Name On Anything")
+// alert(input.toUpperCase())
+
+// Question No 02
+//  var input = prompt("Enter your Name On Anything")
+// alert(input.toLowerCase())
+
+
+// Strings: measuring length and extracting parts
+// Question No 01
+
+// var favMobile = prompt("Enter Your Fav Mobile Name")
+// document.write(favMobile.length)
+
+// Question No 02
+// var userName = prompt("Enter Your Name")
+// document.write(userName.lastIndexOf())
+
+
+// Strings: finding segments
+// Question No 1
+// var country = "Pakistani"
+// alert(country.indexOf("n"))
+
+// Question No 2
+// var userName = prompt("Enter Your Name");
+// if(userName == "@" || "!"){
+//     alert("Please Enter a valid UserName")
+// }
+// else{
+//     alert("Valid UserName Go For Next")
+// }
+
+
+
+// Strings: finding a character at a location
+// Question No 1
+// var city = "pakistan";
+// alert(city.indexOf("i"))
+
+
+
+// Strings: replacing characters
+// question no 1
+// var city = "hayderabad";
+// var last = city.slice(6)
+// var first = "islam"
+// alert(first+last)
+
+
+
+// Rounding Numbers
+// Question No 1
+// var input = prompt("Enter Number")
+// alert(Math.round(input))
+// alert(Math.floor(input))
+// alert(Math.ceil(input))
+
+
+// Question No 2
+// var input = prompt("Enter Negative Float Number")
+// alert(Math.round(input))
+// alert(Math.floor(input))
+// alert(Math.ceil(input))
+
+
+
+// Generating Random Number
+// console.log(Math.random())
+
+
+// // Question No 2
+// var tose = prompt("Tose Select 2 or 1")
+// var value = Math.random();
+// var converting = value*2
+// var round = Math.round(converting)
+// console.log(round)
+
+// if(round == "1"){
+//     alert("Head's Win")
+// }
+// else if(round == "2"){
+//     alert("Tals's Win")
+// }
+// else{
+//     alert("Confused Try Again")
+// }
+
+
+// Converting strings to numbers, numbers to strings
+// var num = 2345;
+// var string = num.toString()
+// console.log(string)
+
